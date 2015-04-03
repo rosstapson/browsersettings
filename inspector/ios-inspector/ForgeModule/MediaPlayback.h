31ded3a0f7c2af0e10b73f7d0b5e2e74b63d6e95
Ross Tapson <ross.tapson@gmail.com>
2015-04-03 08:40:34 +0200

    added ascii nude ladies.

---
+++ inspector/ios-inspector/ForgeModule/MediaPlayback.h
@@ -0,0 +1,13 @@
+//
+//  MediaPlayback.h
+//  ForgeModule
+//
+//  Created by Ross Tapson on 2015/04/03.
+//  Copyright (c) 2015 Trigger Corp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MediaPlayback : NSObject
+
+@end
---
+++ inspector/ios-inspector/ForgeModule/MediaPlayback.m
@@ -0,0 +1,13 @@
+//
+//  MediaPlayback.m
+//  ForgeModule
+//
+//  Created by Ross Tapson on 2015/04/03.
+//  Copyright (c) 2015 Trigger Corp. All rights reserved.
+//
+
+#import "MediaPlayback.h"
+
+@implementation MediaPlayback
+
+@end
