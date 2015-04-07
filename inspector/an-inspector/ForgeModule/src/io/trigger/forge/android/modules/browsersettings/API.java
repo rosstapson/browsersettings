93a0c707df104daade980a84ac2d87cbace85a3a
rosstapson <ross.tapson@gmail.com>
2015-04-07 14:31:04 +0200

    added some android bitties.

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/API.java
@@ -5,18 +5,23 @@ import io.trigger.forge.android.core.ForgeParam;
 import io.trigger.forge.android.core.ForgeTask;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+//import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class API {
 	public static void showAlert(final ForgeTask task, @ForgeParam("text") final String text) {
-		if (text.length() == 0) {
-			// Error if there is no text to show
-			task.error("No text entered");
-			return;
-		}
+		
+		
 		task.performUI(new Runnable() {
 			public void run() {
+				JsonObject configObject = (JsonObject)ForgeApp.configForModule("browsersettings").get("media_playback");
+				boolean inlineVideo = configObject.get("inline_video").getAsBoolean();
+				boolean userActionRequired = configObject.get("require_user_action").getAsBoolean();
+				
+				
+				
 				AlertDialog.Builder builder = new AlertDialog.Builder(ForgeApp.getActivity());
-				builder.setMessage(text).setCancelable(false).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				builder.setMessage("inline video: " + inlineVideo + " / user action required: " + userActionRequired).setCancelable(false).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int which) {
 						task.success();
 					}
