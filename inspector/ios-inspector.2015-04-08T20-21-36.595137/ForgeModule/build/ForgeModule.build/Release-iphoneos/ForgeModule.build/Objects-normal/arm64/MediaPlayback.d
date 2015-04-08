ac3e419158a4633d42ac21d6a7c1bc97ea6e8261
Ross Tapson <ross.tapson@gmail.com>
2015-04-08 20:23:18 +0200

    removed loggin bitties.

---
+++ inspector/ios-inspector.2015-04-08T20-21-36.595137/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/arm64/MediaPlayback.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ross/forge-workspace/modules/browsersettings/inspector/ios-inspector/ForgeModule/MediaPlayback.m \
+  /Users/ross/forge-workspace/modules/browsersettings/inspector/ios-inspector/ForgeModule/MediaPlayback.h
---
+++ inspector/ios-inspector.2015-04-08T20-21-36.595137/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/arm64/browsersettings_EventListener.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/ross/forge-workspace/modules/browsersettings/inspector/ios-inspector/ForgeModule/browsersettings_EventListener.m \
+  /Users/ross/forge-workspace/modules/browsersettings/inspector/ios-inspector/ForgeModule/browsersettings_EventListener.h \
+  /Users/ross/forge-workspace/modules/browsersettings/inspector/ios-inspector/ForgeModule/MediaPlayback.h
