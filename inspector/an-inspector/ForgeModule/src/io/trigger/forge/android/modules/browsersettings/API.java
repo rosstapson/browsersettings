95a6a1e48514ef351126881ce69a3151703c7390
rosstapson <ross.tapson@gmail.com>
2015-05-05 13:47:01 +0200

    java bitties review

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/API.java
@@ -1,5 +1,5 @@
 package io.trigger.forge.android.modules.browsersettings;
-
+/*
 import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeParam;
 import io.trigger.forge.android.core.ForgeTask;
@@ -7,7 +7,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 //import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
+*/
 public class API {
 	
 }
--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/Util.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/Util.java
@@ -1,13 +1,13 @@
 package io.trigger.forge.android.modules.browsersettings;
 
 import io.trigger.forge.android.core.ForgeApp;
-import io.trigger.forge.android.core.ForgeLog;
+//import io.trigger.forge.android.core.ForgeLog;
 
 public class Util {
 	public static void setInlineMedia(boolean forceInlineMedia) {
-		ForgeLog.i("ross: inline video: " + forceInlineMedia);
+	    //ForgeLog.i("ross: inline video: " + forceInlineMedia);
 		//not required for android, video plays inline by default
-		
+	
 	}
 	public static void setUserActionRequired(boolean userActionRequired) {
 		
